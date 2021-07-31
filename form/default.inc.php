f3797c6c0db277c33dd577704af0f69966b57d23
Kaitlin Shumake <shumakek@ufl.edu>
2021-07-31 10:25:28 -0400

    Added PHP content

---
+++ form/default.inc.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+      <ul>
+        <li><a href="index.html">home</a></li>
+        <li><a href="contact.html">contact</a></li>
+        <li><a href="about.html">about me</a></li>
+      </ul>
+    </nav>
+</header>
+
+<div class="container">
+  <h1>Sorry, something went wrong with your submission.</h3>
+    <p>Please try again.</p>
